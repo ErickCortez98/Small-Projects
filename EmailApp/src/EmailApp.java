2d839e959183cd25f44de10cda30908b62f1d416
ErickCortez98 <erick-cortez98@utexas.edu>
2020-07-16 17:52:18 -0500

    Added Banking and Email simple programs

---
+++ EmailApp/src/EmailApp.java
@@ -0,0 +1,10 @@
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email em1 = new Email("Erick", "Cortez");
+		System.out.print(em1.showInfo());
+	}
+
+}
